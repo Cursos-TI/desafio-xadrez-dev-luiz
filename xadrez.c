5fe1cbd5425173f953467d7c076d107dc70b3b20
Luiz Eduardo <138223429+dev-luiz@users.noreply.github.com>
2025-10-16 14:30:06 +0000

    Commit final - desafio de movimentação Xadrz NOVATO

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,30 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Variáveis
+    int torre = 0, rainha = 0;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Movimentação do bispo
+    for(int bispo = 0; bispo <= 4; bispo++){
+        printf("Bispo: cima direita\n");
+    }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
+    // Movimentação da torre
+    do
+    {
+        printf("Torre: cima\n");
+        torre++;
+    } while (torre <= 4);
+    
+    // Movimentação da rainha
+    while (rainha <= 7)
+    {
+        printf("Rainha: esquerda\n");
+        rainha++;
+    }
+    
+    // Exibe mensagem final de encerramento do processo
+    printf("Movimentos concluídos\n");
     return 0;
 }
