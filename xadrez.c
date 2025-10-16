92105c9a0975267eb6653279ae457c3aeb8568cf
Luiz Eduardo <138223429+dev-luiz@users.noreply.github.com>
2025-10-16 18:13:07 +0000

    Commit final desafio de xadrez nível mestre

--- xadrez.c
+++ xadrez.c
@@ -4,41 +4,73 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void moverTorre(int c){ // Movimentação da torre (5 casas para cima)
+
+    do
+    {
+        printf("Torre: direita\n");
+        c--;
+    } while (c > 0);
+
+}
+
+void moverRainha(int c){ // Movimentação da rainha (08 casas para esquerda)
+
+    while (c > 0)
+    {
+        printf("Rainha: esquerda\n");
+        c--;
+    } 
+}
+
+void moverBispo(int c){ // Movimentação do bispo (cinco casas na diagonal direita)
+    
+    for(c; c > 0 ; c--){ // Loop aninhado para o Bispo
+        printf("Bispo: cima\n");
+        for(int i = c; i == c; i--){
+
+            int casaAtual = 0;
+
+            casaAtual = i == 5 ? 1 : casaAtual; // Facilita a visualização da casa atual que o Bispo está movendo
+            casaAtual = i == 4 ? 2 : casaAtual;
+            casaAtual = i == 3 ? 3 : casaAtual;
+            casaAtual = i == 2 ? 4 : casaAtual;
+            casaAtual = i == 1 ? 5 : casaAtual;
+
+            printf("Bispo: direita [º%d casa]\n", casaAtual);
+        }
+    }
+}
+
+void moverCavalo(int c){ //Movimentação do cavalo (duas casas para cima e uma para direita)
+
+    for(c; c >= 3; c--){
+
+        printf("---------------------------------------\n"); // Separador branco
+        for(int i = 0, j = c; i < j; i++){
+            printf("Cavalo: cima\n");
+            j--;
+        }
+
+        printf("Cavalo: direita\n");
+
+    }
+}
+
 int main() {
 
     // Variáveis
-    int torre = 0, rainha = 0, cavalo, bispo;
-
-    // Movimentação do bispo (cinco casas na diagonal direita)
-    for(bispo = 0; bispo <= 4; bispo++){
-        printf("Bispo: cima direita\n");
-    }
-
-    // Movimentação da torre (5 casas para cima)
-    do
-    {
-        printf("Torre: cima\n");
-        torre++;
-    } while (torre <= 4);
+    int casasTorre, casasRainha, casasBispo, casasCavalo;
     
-    // Movimentação da rainha (08 casas para esquerda)
-    while (rainha <= 7)
-    {
-        printf("Rainha: esquerda\n");
-        rainha++;
-    }
-    
-    // Separador branco
-    printf("---------------------------------------\n");
+    casasTorre = 5;
+    casasRainha = 8;
+    casasBispo = 5;
+    casasCavalo = 3;
 
-    //Movimentação do cavalo (duas casas para baixo e uma para esquerda)
-    for(cavalo = 0; cavalo < 1; cavalo++){
-        for(int i = 0; i <= 1; i++){
-            printf("Cavalo: baixo\n");
-        }
-
-        printf("Cavalo: esquerda\n");
-    }
+    moverTorre(casasTorre);
+    moverRainha(casasRainha);
+    moverBispo(casasBispo);
+    moverCavalo(casasCavalo);
 
     // Exibe mensagem final de encerramento do processo
     printf("Movimentos concluídos\n");
