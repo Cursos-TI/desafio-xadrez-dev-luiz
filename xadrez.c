38093457b75e707cbc01d3fc17703dbbbe28b3a7
Luiz Eduardo <138223429+dev-luiz@users.noreply.github.com>
2025-10-16 16:29:21 +0000

    Commit final desafio de xadrez nível AVENTUREIRO

--- xadrez.c
+++ xadrez.c
@@ -7,27 +7,39 @@
 int main() {
 
     // Variáveis
-    int torre = 0, rainha = 0;
+    int torre = 0, rainha = 0, cavalo, bispo;
 
-    // Movimentação do bispo
-    for(int bispo = 0; bispo <= 4; bispo++){
+    // Movimentação do bispo (cinco casas na diagonal direita)
+    for(bispo = 0; bispo <= 4; bispo++){
         printf("Bispo: cima direita\n");
     }
 
-    // Movimentação da torre
+    // Movimentação da torre (5 casas para cima)
     do
     {
         printf("Torre: cima\n");
         torre++;
     } while (torre <= 4);
     
-    // Movimentação da rainha
+    // Movimentação da rainha (08 casas para esquerda)
     while (rainha <= 7)
     {
         printf("Rainha: esquerda\n");
         rainha++;
     }
     
+    // Separador branco
+    printf("---------------------------------------\n");
+
+    //Movimentação do cavalo (duas casas para baixo e uma para esquerda)
+    for(cavalo = 0; cavalo < 1; cavalo++){
+        for(int i = 0; i <= 1; i++){
+            printf("Cavalo: baixo\n");
+        }
+
+        printf("Cavalo: esquerda\n");
+    }
+
     // Exibe mensagem final de encerramento do processo
     printf("Movimentos concluídos\n");
     return 0;
